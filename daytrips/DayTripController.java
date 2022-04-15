b8625e467b6459decd901b61b1162e71840cf56d
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2022-04-15 13:08:39 +0000

    bookTrip function á frumstigi og hringlaði aðeins til í schema.

--- daytrips/DayTripController.java
+++ daytrips/DayTripController.java
@@ -3,9 +3,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.Set;
+import java.util.UUID;
 import java.sql.SQLException;
+
+import javax.sql.ConnectionEvent;
 import javax.sql.rowset.CachedRowSet;
 
 class DayTripController {
@@ -84,11 +88,83 @@ class DayTripController {
      Decode parameters... */
   }
 
+  /** Method makes a new booking object and inserts it into database.
+   * 
+   * @param dayTrip DayTrip object that is being booked.
+   * @param clientSSN SSN of the client booking the trip.
+   * @param isPaid  whether client has paid or not.
+   * @return  booking id for lookup in database.
+   */
+  public static String bookDayTrip(
+    DayTrip dayTrip, 
+    String clientSSN, 
+    String clientEmail,
+    String clientPhoneNumber,
+    int clientCount, 
+    Boolean isPaid) {
+      UUID bookingID = UUID.randomUUID();
+      System.out.println(bookingID);
+
+
+      Booking b = new Booking(
+        bookingID, 
+        clientSSN, 
+        clientEmail, 
+        clientPhoneNumber, 
+        clientCount,
+        dayTrip, 
+        dayTrip.getDate(), 
+        isPaid
+        );
+      
+        String query = "insert into booking(ID, Client, DayTrip, Date, IsPaid, ClientEmail, ClientPhoneNumber, ClientCount) "
+          + "values(?, ?, ?, ?, ?, ?, ?, ?);";
+
+        ArrayList<String> values = new ArrayList<>();
+        
+        values.add(b.getBookingId().toString());
+        values.add(b.getClient());
+        values.add(b.getDayTrip().getTripName());
+        values.add(b.getLocalDate().toString());
+        values.add(String.valueOf(b.isPaid()));
+        values.add(b.getClientEmail());
+        values.add(b.getClientPhoneNumber());
+        values.add(Integer.toString(b.getClientCount()));
+        for(String s: values) System.out.println(s);
+
+        try {
+          Query.insert(query, values);
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+
+          
+      return b.getBookingId().toString();
+    }
+
   public static void main(String[] args) {
     Hashtable<String, Object> params = new Hashtable<>();
     params.put("difficulty", 3);
     params.put("capacity", 5);
 
     getDayTrips(params);
+
+    LocalDate ld = LocalDate.now();
+    LocalDateTime ldt1 = LocalDateTime.of(2022, 6, 12, 20, 00);
+    LocalDateTime ldt2 = LocalDateTime.of(2022, 6, 12, 22, 00);
+    DayTrip d = new DayTrip(
+      "Frostahlaup",
+       ld,
+        ldt1,
+         ldt2,
+          2,
+           "Bara gaman",
+            12,
+             3000,
+              "Ævintýraferðir",
+               "Vík í Mýrdal",
+                5);
+    
+    bookDayTrip(d, "300321-2240", "frosti@iceman.is", "000-0000", 3, true);
   }
 }
