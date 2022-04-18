3c22b4ebfcb5f6fe05dc43948f8fa9176b3a6987
Óskar Berenguer <oscar6662@Oskars-MacBook-Air.local>
2022-04-18 20:50:59 +0000

    cron job set up

--- daytrips/EmailSender.java
+++ daytrips/EmailSender.java
@@ -1,9 +1,22 @@
-import java.util.UUID;
+package daytrips;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-class EmailSender {
+public class EmailSender extends TimerTask {
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             System.out.println(i + " = " + UUID.randomUUID().toString());
         }
+        Timer t = new Timer();
+        EmailSender email = new EmailSender();
+        Long time = new Date().getTime();
+        Date date = new Date(time - time % (24 * 60 * 60 * 1000));
+        t.scheduleAtFixedRate(email, new Date(date.getTime() + 24 * 60 * 60 * 1000), 86400000);
+    }
+
+    @Override
+    public void run() {
+        
     }
 }
